d76e7c36863ea530ef15ce61431bce3400e77a3e
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-09 12:37:53 +0100

    changes things and ordenados

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function() {
-    return gulp.src('sass/**/*.scss')
+    return gulp.src('config/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('dist/css/.'));
+});
+gulp.task('js_bootstrap', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.js'])
+        .pipe(gulp.dest("dist/js/"))
+});
+gulp.task('js_andres', function() {
+    return gulp.src(['config/**/*.js'])
+        .pipe(gulp.dest("dist/js/."))
 });
 
-gulp.task('default', gulp.series(['sass']));
+gulp.task('default', gulp.series(['sass','js_bootstrap','js_andres']));
