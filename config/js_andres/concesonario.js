cc8d6df2f11c1c7d2d8051ee6e40b35bc59e38d6
Andres Garcia Bauza <agarcia15183@alumnes.iesmanacor.cat>
2021-12-25 23:13:18 +0100

    deleted img

--- config/js_andres/concesonario.js
+++ config/js_andres/concesonario.js
@@ -8,20 +8,6 @@ function getConcesonario() {
                 stars: 5,
                 labels: ["new"],
                 img: "img/Cars/Chevrolet/4e5819a0e3ad150375283312694f927452ed943b.png"
-            },
-            {
-                name: "Chevrolet Corvette ZR1 Coupe",
-                price: 119995,
-                stars: 3,
-                labels: ["offer","new"],
-                img: "img/Cars/Chevrolet/SebringOrange.png"
-            },
-            {
-                name: "Chevrolet Corvette Grand Sport 2017",
-                price: 70445,
-                stars: 4,
-                labels: ["blackfriday"],
-                img: "img/Cars/Chevrolet/19Corvette-Z06-bean-500px.png"
             }
         ],
         MCLAREN = [
@@ -32,13 +18,6 @@ function getConcesonario() {
                 stars: 4,
                 labels: ["new","blackfriday"],
                 img: "img/Cars/McLaren/f6a57527-cfda-4454-8761-c8ac0ed50b28.png"
-            },
-            {
-                name: "McLaren 600LT 2018",
-                price:  210000,
-                stars: 5,
-                labels: ["offer"],
-                img: "img/Cars/McLaren/8859a81bd114df94d9f432350c934f4a.png"
             }
         ],
 
@@ -50,20 +29,6 @@ function getConcesonario() {
                 stars: 3,
                 labels: ["new"],
                 img: "img/Cars/Aston%20Martin/AstornMartinDB112017.png"
-            },
-            {
-                name: "Aston Martin DB5 1964",
-                price: 450000,
-                stars: 2,
-                labels: ["blackfriday","new"],
-                img: "img/Cars/Aston Martin/AstonMartinDB51964.png"
-            },
-            {
-                name: "Aston Martin Vulcan 2016",
-                price: 2300000,
-                stars: 5,
-                labels: ["offer","blackfriday","new"],
-                img: "img/Cars/Aston Martin/aston_martin_vulcan.png"
             }
         ],
 
@@ -75,34 +40,6 @@ function getConcesonario() {
                 stars: 5,
                 labels: ["offer"],
                 img: "img/Cars/BMW/BMWi8Coupe.png"
-            },
-            {
-                name: "BMW M4 Convertible 2018",
-                price: 77200,
-                stars: 4,
-                labels: ["blackfriday"],
-                img: "img/Cars/BMW/AA_BBCQQWK.png"
-            },
-            {
-                name: "BMW M2 Competition 2019",
-                price: 74700,
-                stars: 5,
-                labels: ["offer","blackfriday"],
-                img: "img/Cars/BMW/2019-bmw-m2-competition-coupe-angular-front.png"
-            },
-            {
-                name: "BMW X6 M 2016",
-                price:  105783,
-                stars: 3,
-                labels: ["offer","new"],
-                img: "img/Cars/BMW/727afc6a-2e55-4b8a-bf27-5f64ebfa4322.png"
-            },
-            {
-                name: "BMW Z4 M40i 2020",
-                price:  73295,
-                stars: 4,
-                labels: [],
-                img: "img/Cars/BMW/cosySec.png"
             }
         ],
 
@@ -114,13 +51,6 @@ function getConcesonario() {
                 stars: 2,
                 labels: ["blackfriday","new"],
                 img: "img/Cars/Audi/AudiS5Sport.png"
-            },
-            {
-                name: "Audi S5 Sportback 2017",
-                price: 2134100,
-                stars: 5,
-                labels: ["new"],
-                img: "img/Cars/Audi/zALtwV8sHG.png"
             }
         ],
 
@@ -133,27 +63,6 @@ function getConcesonario() {
                 stars: 5,
                 labels: ["blackfriday"],
                 img: "img/Cars/Ferrari/FERRARI_488_PISTA_3.9_V8_TURBO_TRIBUTO_GASOLINA_F1DCT_34845512565446734.png"
-            },
-            {
-                name: "Ferrari LaFerrari 2016",
-                price: 1300000,
-                stars: 2,
-                labels: ["new"],
-                img: "img/Cars/Ferrari/ferrari-laferrari-2016-base-base.png"
-            },
-            {
-                name: "Ferrari FXX-K Evo 2018",
-                price: 2200000,
-                stars: 5,
-                labels: ["offer"],
-                img: "img/Cars/Ferrari/5f687d22eeedf80b2a631c6c-ferrari-fxx-m-design-hotspot-mob-new.png"
-            },
-            {
-                name: "Ferrari Testarossa Coupé 1984",
-                price: 174333,
-                stars: 1,
-                labels: [],
-                img: "img/Cars/Ferrari/HOR_XB1_Ferrari_512.png"
             }
         ],
 
@@ -176,13 +85,6 @@ function getConcesonario() {
                 stars: 5,
                 labels: ["offer"],
                 img: "img/Cars/Ford/2020-Ford-Mustang-Shelby-colors_o8.png"
-            },
-            {
-                name: "Ford Mustang Shelby GT500",
-                price: 440000,
-                stars: 3,
-                labels: ["new"],
-                img: "img/Cars/Ford/HOR_XB1_Ford_GT_17.webp"
             }
         ],
 
